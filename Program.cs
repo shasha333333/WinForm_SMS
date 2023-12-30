1c77c79e6ce0ce46496998e92331d20d0980ac7a
shasha333333 <63197886+shasha333333@users.noreply.github.com>
2023-12-30 21:42:28 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using StudentManageSystem.Forms;
+using StudentManageSystem.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentManageSystem
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+           
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new LoginForm());
+            //Application.Run(new TeacherForm());
+            //Application.Run(new StudentForm());
+            //Application.Run(new AddStudentForm());
+            //SqlHelper.Test();
+
+        }
+    }
+}
